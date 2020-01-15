ba6b50a7424a38a32bd480afad92f914ee4f22e7
Tammaro Impicciatore <t.impicciatore@resi.it>
2020-01-15 11:41:46 +0100

    ciao

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic  
   store_in collection:"user",database:"db",client:"default"
