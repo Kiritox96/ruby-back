10a3da95acc16940ad07573224bb1140d93617d9
Tammaro Impicciatore <t.impicciatore@resi.it>
2020-01-15 11:59:36 +0100

    m

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -16,7 +16,7 @@ class AuthenticateUser
     private
   
     def user
-      user = User.find_by(:email)
+      user = User.find_by(email: @email)
       return user if user && user.authenticate(password)
   
       errors.add :user_authentication, 'Invalid credentials'
