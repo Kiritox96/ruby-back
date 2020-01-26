096ac5f6cee46bddc6f4c023acd212f4d8671af1
EC2 Default User <ec2-user@ip-172-31-39-114.eu-west-2.compute.internal>
2020-01-26 18:55:38 +0000

    restore file

--- app/models/file.rb
+++
@@ -1,7 +0,0 @@
-class File 
- include Mongoid::Document
-  store_in collection: "apk", database:"files", client:"default" 
-  # this sets up the model to accept incoming files inside files parameter.
-    has_many_attached :uploads
-
-end
