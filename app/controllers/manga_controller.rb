0d1a8a53ddbf454903785d4e47eb2798553629a6
tammaro <tammaroimpicciatore@hotmail.com>
2020-02-26 18:33:28 +0100

    tramite

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -5,7 +5,10 @@ class MangaController < ApplicationController
     def show
       if params[:id].present?
         @manga = Manga.get_manga(params[:id])
-        render @manga
+        render json: @manga
+      elsif params[:x].present?
+        @manga = Manga.get_page(params[:x])
+        render json: @manga
       else
         render json:{error:'param not found'}
       end
--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,7 +1,7 @@
 class Manga < ActiveRestClient::Base
   base_url Rails.application.config.api_server_url
 
-
+  get :get_page, "/list/1/?p=:x"
 
   get :get_manga, "/manga/:id"
 end
