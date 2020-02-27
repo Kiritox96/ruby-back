8f275a638541b8b556da8af5a7b4ecf46234e51f
tammaro <tammaroimpicciatore@hotmail.com>
2020-02-27 14:14:43 +0100

    ciao

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -1,27 +1,18 @@
 
 class MangaController < ApplicationController
 
-  def fetch(uri_str, limit = 10)
-    # You should choose better exception.
-    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
-  
-    url = URI.parse(uri_str)
-    req = Net::HTTP::Get.new(url.path, {
-      'User-Agent' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5'
-    })
-    response = Net::HTTP.start(url.host, url.port, use_ssl: true) { |http| http.request(req) }
-    case response
-    when Net::HTTPSuccess     then response
-    when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-    else
-      response.error!
-    end
-  end
-
     def show
       if params[:id].present?
         source = Manga.base_url + '/api/manga/' + params[:id]
-        render json: fetch(source, 5)
+        agent = Mechanize.new
+        begin
+          response = @agent.get(source)
+          render json: response
+          rescue Mechanize::ResponseCodeError 
+          rescue Timeout::Error
+          rescue Mechanize::RedirectLimitReachedError
+          rescue StandardError
+        end
       else
         render json:{error:'param not found'}
       end
