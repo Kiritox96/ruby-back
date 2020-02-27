7bd9ad4a7f882150ac2805a8ef4727aec6b9545b
tammaro <tammaroimpicciatore@hotmail.com>
2020-02-27 13:54:54 +0100

    ciao

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -6,8 +6,8 @@ class MangaController < ApplicationController
     def show
       if params[:id].present?
         response = Net::HTTP.get_response(Manga.base_url, '/api/manga/' + params[:id])
-        if response.body
-          render json:response.body
+        if response
+          render response.body
         else
           render json:{error:'manga not found'}
         end
