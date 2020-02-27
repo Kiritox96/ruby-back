41e227ca8c317f908f1a291e1f7fa4597bb6e2aa
tammaro <tammaroimpicciatore@hotmail.com>
2020-02-27 14:08:38 +0100

    ciao

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -1,19 +1,25 @@
 
 class MangaController < ApplicationController
 
-
+  def fetch(uri_str, limit = 10)
+    # You should choose better exception.
+    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+  
+    url = URI.parse(uri_str)
+    req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (etc...)' })
+    response = Net::HTTP.start(url.host, url.port, use_ssl: true) { |http| http.request(req) }
+    case response
+    when Net::HTTPSuccess     then response
+    when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+    else
+      response.error!
+    end
+  end
 
     def show
       if params[:id].present?
         source = Manga.base_url + '/api/manga/' + params[:id]
-        resp = Net::HTTP.get_response(URI.parse(source))
-        data = resp.body
-        result = JSON.parse(data)
-        if result
-          render result
-        else
-          render json:{error:'manga not found'}
-        end
+        render json: fetch(source, 5)
       else
         render json:{error:'param not found'}
       end
