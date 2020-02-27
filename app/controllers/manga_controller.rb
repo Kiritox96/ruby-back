56fecb70f6b6bc775f7b12e46af3bb45eea0d759
tammaro <tammaroimpicciatore@hotmail.com>
2020-02-27 14:23:53 +0100

    ciao

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -2,16 +2,29 @@
 class MangaController < ApplicationController
 
     def show
-      if params[:id].present?
-        source = Manga.base_url + '/api/manga/' + params[:id]
-        @agent = Mechanize.new
-        begin
-          response = @agent.get(source)
-          render json: response.body
-          rescue Mechanize::ResponseCodeError 
-          rescue Timeout::Error
-          rescue Mechanize::RedirectLimitReachedError
-          rescue StandardError
+      if params[:id]-present? 
+        if params[:manga] == '1'
+          source = Manga.base_url + '/api/manga/' + params[:id]
+          @agent = Mechanize.new
+          begin
+            response = @agent.get(source)
+            render json: response.body
+            rescue Mechanize::ResponseCodeError 
+            rescue Timeout::Error
+            rescue Mechanize::RedirectLimitReachedError
+            rescue StandardError
+          end
+        elsif params[:capitolo] == '1'
+          source = Manga.base_url + '/api/chapter/' + params[:id]
+          @agent = Mechanize.new
+          begin
+            response = @agent.get(source)
+            render json: response.body
+            rescue Mechanize::ResponseCodeError 
+            rescue Timeout::Error
+            rescue Mechanize::RedirectLimitReachedError
+            rescue StandardError
+          end
         end
       else
         render json:{error:'param not found'}
